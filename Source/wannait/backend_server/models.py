5f918de77dbe88c82f0f866bbd23597c75ee07d6
L1ttl3S1st3r <kudirko.anton@gmail.com>
2019-11-21 12:38:35 +0300

    Fixed models foreign fields

--- Source/wannait/backend_server/models.py
+++ Source/wannait/backend_server/models.py
@@ -6,11 +6,12 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ('id', 'email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
 
 class BackendProduct(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='BackendProduct', on_delete=models.CASCADE, default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=2)
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=1000)
     image_url = models.URLField(max_length=1000)
@@ -21,15 +22,17 @@ class BackendProduct(models.Model):
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
     class Meta:
         model = BackendProduct
-        fields = ['name', 'id', 'image_url', 'description']
+        fields = ['owner', 'name', 'id', 'image_url', 'description']
 
 
 class BackendComment(models.Model):
     id = models.IntegerField(primary_key=True)
     product = models.ForeignKey(BackendProduct, on_delete=models.CASCADE)
-    user = models.ForeignKey('auth.User', related_name='BackendComment', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=2)
     text = models.CharField(max_length=1000)
 
     def __str__(self):
@@ -37,18 +40,22 @@ class BackendComment(models.Model):
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
     class Meta:
         model = BackendComment
-        fields = ['product', 'id', 'text']
+        fields = ['user', 'product', 'id', 'text']
 
 
 class BackendLike(models.Model):
     id = models.IntegerField(primary_key=True)
     product = models.ForeignKey(BackendProduct, on_delete=models.CASCADE)
-    user = models.ForeignKey('auth.User', related_name='BackendLike', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=2)
 
 
 class LikeSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
     class Meta:
         model = BackendLike
-        fields = ['product', 'id']
+        fields = ['user', 'product', 'id']
